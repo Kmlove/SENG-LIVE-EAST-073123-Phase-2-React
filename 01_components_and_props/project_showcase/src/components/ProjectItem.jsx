965fd58333c7e1ac25ec8dea5b7fe447de527f42
Kim Love <kimberlymlove15@gmail.com>
2023-08-21 14:22:47 -0400

    Adds componts folder to contain all components for our app

---
+++ 01_components_and_props/project_showcase/src/components/ProjectItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function ProjectItem(props) {
+    console.log(props)
+    console.log(props.projects)
+
+    return (
+    <div>ProjectItem</div>
+  )
+}
---
+++ 01_components_and_props/project_showcase/src/components/ProjectList.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+// Don't need to use the line above anymore
+
+// type rfc to get a shortcut to create a functional component
+
+//Using inline export here
+export default function ProjectList() {
+  return (
+    <div>ProjectList</div>
+  )
+}
