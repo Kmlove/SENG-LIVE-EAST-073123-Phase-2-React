16c5537c7f087bb886d672b6357fcbf307d00f84
Kim Love <kimberlymlove15@gmail.com>
2023-08-21 14:24:11 -0400

    Adds components to App component

--- 01_components_and_props/project_showcase/src/App.js
+++ 01_components_and_props/project_showcase/src/App.js
@@ -1,5 +1,14 @@
+import Header  from "./components/Header";
+import ProjectList from './components/ProjectList'
+import projects from './projects'
+
 function App() {
-  return <div className="App">Project showcase</div>;
+  return (
+    <div className="App">Project showcase
+      <Header />
+      <ProjectList projects={projects}/>
+    </div>
+  )
 }
 
 export default App;
